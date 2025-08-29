9045b81084b73267a4295f17091a4940f76dd5b3
mickstarego <mds8dp@virginia.edu>
2025-08-29 13:06:44 -0400

    rename public to docs:

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'docs' directory
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
