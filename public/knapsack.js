29ff011c35e0389a536331da1e683997fb8f1dd1
mickstarego <mds8dp@virginia.edu>
2025-08-29 13:04:26 -0400

    finished implementation

---
+++ public/knapsack.js
@@ -0,0 +1,181 @@
+self.onmessage = function (event) {
+    let result;
+    try {
+        result = run(event.data.target, event.data.tax, event.data.itemList);
+    } catch (e) {
+        result = { error: e.message };
+    }
+    self.postMessage(result);
+};
+
+function run(target, tax, itemList) {
+    // Multiply prices by 100 to get integer value
+    // Sort items by price, i.e [78, 105, 199]
+    const calculatedItems = [...itemList]
+        .map((item) => ({...item, integerPrice: Math.trunc(item.unitPrice * 100)}))
+        .sort(((a, b) => a.integerPrice - b.integerPrice));
+
+    // Calculate the target value
+    //
+    // target = round(subtotal * (1 + tax), 2)
+    // ==> target - 0.005 = subtotal * (1 + tax)
+    //     subtotal >= (target - 0.005) / (1 + tax)
+    // ==> target + 0.005 = subtotal * (1 + tax)
+    //     subtotal < (target + 0.005) / (1 + tax)
+    const integerTarget = target * 100;
+    const rawTargetMin = (integerTarget - 0.5) / (1 + (tax / 100));
+    const rawTargetMax = (integerTarget + 0.5) / (1 + (tax / 100));
+
+    // Some combinations of target and tax are not solvable
+    if (Math.ceil(rawTargetMin) >= rawTargetMax) {
+        throw new Error(`Target ${target} is not possible for tax ${tax.toFixed(2)}%`);
+    }
+    const calculatedTarget = Math.ceil(rawTargetMin);
+
+    // Construct 2D array of data
+    //   - Rows represent number of items included [0, itemList.length]
+    //   - Cols represent total price [0, calculatedTarget]
+    //   - Entries (true/false) indicate whether it is possible
+    //     to make that specific price with the given items
+    const data = [];
+
+    // Populate the base case for 0 items
+    const baseCol = new Array(calculatedTarget + 1).fill(false);
+    baseCol[0] = true;
+    data.push(baseCol);
+
+    // Build array while progressively increasing item count
+    for (let row = 1; row <= calculatedItems.length; row++) {
+        const col = new Array(calculatedTarget + 1).fill(false);
+        for (let price = 0; price <= calculatedTarget; price++) {
+            // The item that wasn't included in the previous row
+            const newItem = calculatedItems[row - 1];
+
+            // Given price P, new item I, and number of new items N,
+            // check for (P - N * I.price) in the previous row to determine
+            // if the current combination of P, I and N is possible.
+            let newItemCount = 0;
+            while (newItemCount <= getNewItemCountMax(price, newItem.integerPrice)) {
+                const prevTarget = price - newItemCount * newItem.integerPrice;
+                if (data[row - 1][prevTarget]) {
+                    col[price] = true;
+                    break;
+                }
+                newItemCount++;
+            }
+        }
+        data.push(col);
+    }
+    
+    // Check if solutions exist
+    const [lastRow, lastPrice] = [calculatedItems.length, calculatedTarget];
+    if (!data[lastRow][lastPrice]) {
+        // No solutions found!
+        return {
+            data: [],
+            numSolutions: 0
+        };
+    }
+
+    // Use DFS to read solutions
+    const solutions = [];
+    const stack = [[lastRow, lastPrice, []]];
+    while (stack.length) {
+        const [row, price, path] = stack.pop();
+        const newItem = calculatedItems[row - 1];
+
+        // Solution found!
+        if (row === 0 && price === 0) {
+            solutions.push(path);
+            continue;
+        }
+
+        // Push children onto stack
+        let newItemCount = 0;
+        while (newItemCount <= getNewItemCountMax(price, newItem.integerPrice)) {
+            const prevTarget = price - newItemCount * newItem.integerPrice;
+            if (data[row - 1][prevTarget]) {
+                stack.push([row - 1, prevTarget, [newItemCount, ...path]]);
+            }
+            newItemCount++;
+        }
+    }
+
+    // Construct resulting item list from solutions
+    const results = solutions.map((path) => path
+        .map((quantity, i) => ({
+            ...calculatedItems[i],
+            quantity,
+            totalPrice: calculatedItems[i].unitPrice * quantity
+        }))
+        .filter((item) => item.quantity)
+    );
+
+    // Score all and pick the best result
+    let bestResult;
+    let bestScore = 0;
+    results.forEach((result) => {
+        const score = getScore(result, calculatedItems, calculatedTarget);
+        if (score > bestScore) {
+            bestScore = score;
+            bestResult = result;
+        }
+    });
+
+    // Sort the result by category and price
+    bestResult.sort((a, b) =>
+        a.category === b.category
+            ? b.totalPrice - a.totalPrice
+            : a.category.localeCompare(b.category)
+    );
+
+    // Add tax and total
+    const subtotal = bestResult.reduce((prev, curr) => prev + curr.totalPrice, 0);
+    const totalTax = subtotal * tax / 100;
+    bestResult = [
+        ...bestResult,
+        {
+            name: 'Sales Tax ' + tax.toFixed(2) + '%',
+            totalPrice: totalTax
+        },
+        {
+            name: 'Total',
+            totalPrice: subtotal + totalTax
+        }
+    ];
+
+    return {
+        data: bestResult,
+        numSolutions: results.length
+    };
+}
+
+function getNewItemCountMax(price, newItemPrice) {
+    return Math.floor(price / newItemPrice);
+}
+
+function getScore(result, calculatedItems, calculatedTarget) {
+    // Diversity: Ratio of potential items included
+    const numUniqueItemsIncluded = result.length;
+    const totalUniqueItems = calculatedItems.length;
+    const diversityScore = numUniqueItemsIncluded / totalUniqueItems;
+
+    // Cost efficiency: Total number of items included
+    const numItemsIncluded = result.reduce((prev, curr) => prev + curr.quantity, 0);
+    const minItemPrice = Math.min(...calculatedItems.map((item) => item.integerPrice));
+    const maxItemsIncluded = Math.floor(calculatedTarget / minItemPrice);
+    const efficiencyScore = numItemsIncluded / maxItemsIncluded;
+
+    // Category utilization: Evenness of category representation
+    const categories = new Set(calculatedItems.map((item) => item.category));
+    const categoryCounts = [...categories].map(
+        (category) => result
+            .filter((item) => item.category === category)
+            .reduce((prev, curr) => prev + curr.quantity, 0)
+    );
+    const minSubtotal = Math.min(...categoryCounts);
+    const maxSubtotal = Math.max(...categoryCounts);
+    const utilizationScore = 1 - ((maxSubtotal - minSubtotal) / result.length);
+
+    return (3 * diversityScore) + efficiencyScore + (2 * utilizationScore);
+}
